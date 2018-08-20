06077669cb0cf555e6a91bd1a56ff066c4fc2889
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-08-20 15:44:44 -0700

    Added home page and controllers

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 // require stuff
 var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 
 
 // declare variables
 var app = express();
 
 // set and use statments
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+
+// include controllers/routers
+app.use('/articles', require('./controllers/articles'));
+app.use('/authors', require('./controllers/authors'));
 
 
 //define routes
 app.get('/', function(req, res){
-  res.send('home page!');
+  res.render('home');
 });
 
 // hey listen
