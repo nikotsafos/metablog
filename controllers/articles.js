8478b6b69ac9f99f6c850dc2dd3c4045a957eb28
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-08-20 22:39:10 -0700

    added forms to create new authors and articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.send('Hello world - articles');
+  res.render('articles/index');
 })
+
+router.get('/new', function(req, res){
+  res.render('articles/new');
+});
+
+router.get('/:id', function(req, res) {
+  res.send('article show page goes here');
+});
+
+router.post('/', function(req, res) {
+  res.send('/articles post route reached');
+});
+
 
 module.exports = router;
--- controllers/authors.js
+++ controllers/authors.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-  res.send('Hello world - authors');
-})
+  res.render('authors/index');
+});
+
+router.get('/new', function(req, res){
+  res.render('authors/new');
+});
+
+router.get('/:id', function(req, res) {
+  res.send('suthor show page goes here');
+});
+
+router.post('/', function(req, res) {
+  res.send('/authors post route reached');
+});
 
 module.exports = router;
